c5acbcf8960c13221266eae548dbdb063fbf617a
Stacy Devino <stacy.devino@stitchfix.com>
2021-05-02 18:37:49 -0500

    Goodies and Fun of Stage 2, extras and examples with Kotlin Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+
 // Declare Plugins in a Block
 plugins {
     id("com.android.application")
@@ -21,7 +24,7 @@ android {
         applicationId = "com.stacydevino.videoplayertypes"
         minSdkVersion(22)
         targetSdkVersion(30)
-        versionCode = 1
+        versionCode = timestampVersionCode() // Dynamically generated Timestamps!
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -54,9 +57,20 @@ android {
 
 dependencies {
 
+    // Enforce configurations to avoid versioning conflicts with 3rd party libs
+    configurations.all {
+        // Enforce latest android support annotations for all dependencies
+        resolutionStrategy.force("com.android.support:support-annotations:1.0.0")
+
+        //This subdependency is included in the Android SDK and can easily cause conflicts
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
+
+
     //General, JIC you include a local library
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 
+    //Kotlin
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.32")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.32")
 
@@ -65,11 +79,17 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
 
+    // Material
+    implementation("com.google.android.material:material:1.3.0")
+
     //Testing
     testImplementation("androidx.arch.core:core-testing:2.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    testImplementation("io.mockk:mockk:1.11.0")
+    androidTestImplementation("io.mockk:mockk:1.11.0")
+    androidTestImplementation("io.mockk:mockk-android:1.11.0")
 
     //Exoplayer
     implementation("com.google.android.exoplayer:exoplayer-core:2.13.3")
@@ -81,8 +101,37 @@ dependencies {
     // Notice that Platforms can be declared inline
     implementation(platform("com.google.firebase:firebase-bom:27.1.0"))
     implementation("com.google.firebase:firebase-perf-ktx")
+
+    // Debug
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
+}
+
+// Helpful error handling and build speed
+kapt {
+    useBuildCache = true
+    correctErrorTypes = true
 }
 
 configurations.all {
     resolutionStrategy.cacheDynamicVersionsFor(0, "seconds")
+}
+
+// Kotlin Functions!
+fun timestampVersionCode(): Int {
+    val dateStampInt = Integer.parseInt(SimpleDateFormat("yyyyMMddHH").format(Date()))
+    println("\nDatestamp version code generated: $dateStampInt\n")
+    return dateStampInt
+}
+
+// Adding Dokka Autogen Docs
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
+    //Sets output directory to app/dokka instead of the default app/build
+    outputDirectory.set(File(project.projectDir, "dokka"))
+
+    // General default Android configuration
+    dokkaSourceSets {
+        configureEach {
+            noAndroidSdkLink.set(false)
+        }
+    }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // TODO : Bintray
+        // TODO : Bintray still tied to jCenter
         maven {
             url = uri("https://dl.bintray.com/kotlin/kotlin-eap")
             content {
@@ -15,11 +15,14 @@ buildscript {
             }
         }
         maven { url = uri ("https://dl.bintray.com/kotlin/kotlinx")} // TODO : Bintray
+        maven{ url = uri("https://dl.bintray.com/kotlin/dokka")} // TODO : Bintray
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.google.firebase:perf-plugin:1.3.5")
+        //Dokka Support for autogen Documentation
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     }
 }
 
@@ -27,7 +30,19 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri ("https://dl.bintray.com/kotlin/kotlinx")}
+        maven { url = uri ("https://dl.bintray.com/kotlin/kotlinx")} // TODO : Bintray
+        maven{ url = uri("https://dl.bintray.com/kotlin/dokka")} // TODO : Bintray
+    }
+}
+
+// Set each module in the project to use Java 8
+// and remove compiler warnings for ExperimentalCoroutinesApi automatically
+// (so you don't need to annotate each)
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs =
+            freeCompilerArgs + "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
     }
 }
 
